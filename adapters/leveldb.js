47bd7d28ece171b0391bce3ef9c847a103f8d08b
AlDoo <alex.doo.gm@gmail.com>
2018-08-22 11:36:31 +0300

    version change compatibility

--- adapters/leveldb.js
+++ adapters/leveldb.js
@@ -1,19 +1,19 @@
 ﻿/*
- 
-This file is part of Streembit application. 
-Streembit is an open source project to create a real time communication system for humans and machines. 
 
-Streembit is a free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+This file is part of Streembit application.
+Streembit is an open source project to create a real time communication system for humans and machines.
+
+Streembit is a free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation, either version 3.0 of the License, or (at your option) any later version.
 
-Streembit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+Streembit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Streembit software.  
+You should have received a copy of the GNU General Public License along with Streembit software.
 If not, see http://www.gnu.org/licenses/.
- 
+
 -------------------------------------------------------------------------------------------------------------------------
-Author: Tibor Z Pardi 
+Author: Tibor Z Pardi
 Copyright (C) 2017 The Streembit software development team
 -------------------------------------------------------------------------------------------------------------------------
 
@@ -22,6 +22,7 @@ Copyright (C) 2017 The Streembit software development team
 'use strict';
 
 const levelup = require('levelup');
+const leveldown = require('leveldown');
 const path = require('path');
 const DbPath = require("./dbpath");
 const logger = require("streembit-util").logger;
@@ -52,7 +53,7 @@ class LevelDbDatabase {
                     // initialize leveldb database
                     var dbpath = new DbPath();
                     var db_path = dbpath.initialize_db_dir(rootdir, type, dbname);
-                    var main_dbobj = levelup(db_path);
+                    var main_dbobj = levelup(leveldown(db_path));
                     this.db = main_dbobj;
                     logger.debug("leveldb " + dbname + " database initialized");
                     resolve();
